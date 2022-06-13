a72ba8b21434e61b7de88fb30f80e6fb4e762cc8
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-06-13 17:28:29 +0900

    Update enter.tsx

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -4,8 +4,8 @@ import Button from "../components/button";
 
 const Enter: NextPage = () => {
   return (
-    <div className="bg-purple-600 w-full h-screen text-3xl font-semibold items-center flex flex-col justify-center gap-y-5">
-      <div className="overflow-hidden relative grid grid-cols-4 gap-1 ">
+    <div className="bg-purple-600 w-full h-screen xl:text-3xl lg:text-2xl md:text-xl sm:text-lg font-semibold items-center flex flex-col justify-center gap-y-5">
+      <div className="overflow-hidden relative grid grid-cols-3 gap-1 ">
         <div className="flex flex-col overflow-hidden whitespace-nowrap items-end text-purple-800 gap-y-2">
           <span>Fortnite VALORANT Fortnite VALORANT</span>
           <span>DJ 포크 얼터너티브 DJ 포크 얼터너티브</span>
@@ -16,7 +16,7 @@ const Enter: NextPage = () => {
           <span>교육 제과 교육 제과</span>
           <span>스탠드업 코미디 뮤지컬 스탠드업 코미디 뮤지컬</span>
         </div>
-        <div className="whitespace-nowrap overflow-visible flex flex-col text-purple-800 gap-y-2 col-span-2">
+        <div className="whitespace-nowrap overflow-visible flex flex-col text-purple-800 gap-y-2 col-span-2 ">
           <div className="flex gap-1 ">
             <p className="text-gray-50">게임</p>
             <span>
@@ -34,6 +34,7 @@ const Enter: NextPage = () => {
           <div className="flex gap-1">
             <p className="text-gray-50">토크쇼</p>
             <span>역사 교육 라디오 게임 뉴스 역사 교육 라디오 게임 뉴스</span>
+            <span>역사 교육 라디오 게임 뉴스 역사 교육 라디오 게임 뉴스</span>
           </div>
           <div className="flex gap-1">
             <p className="text-gray-50">스포츠</p>
@@ -45,35 +46,39 @@ const Enter: NextPage = () => {
           <div className="flex gap-1">
             <p className="text-gray-50">여행 및 야외활동</p>
             <span>Call of Duty : Mobile Free Fire PUBG Mobile Mincraft</span>
+            <span>Call of Duty : Mobile Free Fire PUBG Mobile Mincraft</span>
           </div>
           <div className="flex gap-1">
             <p className="text-gray-50">Just Chatting</p>
+            <span>뉴스 정신 건강 코스프레 뉴스 정신 건강 코스프레</span>
             <span>뉴스 정신 건강 코스프레 뉴스 정신 건강 코스프레</span>
           </div>
           <div className="flex gap-1">
             <p className="text-gray-50">먹방</p>
             <span>소셜이팅 제과 요리 소셜이팅 제과 요리</span>
+            <span>소셜이팅 제과 요리 소셜이팅 제과 요리</span>
           </div>
           <div className="flex gap-1">
             <p className="text-gray-50">특별 이벤트</p>
             <span>스포츠 해설 스탠드업 코미디 스포츠 해설 스탠드업 코미디</span>
+            <span>스포츠 해설 스탠드업 코미디 스포츠 해설 스탠드업 코미디</span>
           </div>
         </div>
         <div></div>
-      </div>
-      <div className="min-w-min max-w-md">
-        <span>Twitch는 항상 새로운 방송으로 가득해요</span>
-        <div className="gap-3 flex my-12 mx-2">
-          <Link href="/profile/signin">
-            <a className="w-full">
-              <Button text="로그인" bgblack bgcolor="bg-purple-500"></Button>
-            </a>
-          </Link>
-          <Link href="/profile/signup">
-            <a className="w-full">
-              <Button text="회원가입" bgcolor="bg-white"></Button>
-            </a>
-          </Link>
+        <div className="w-full text-black md:mt-4 ">
+          <span className="py-3">Twitch는 항상 새로운 방송으로 가득해요</span>
+          <div className="gap-3 flex flex-col my-1 mx-2 md:flex-row md:my-12">
+            <Link href="/profile/signin">
+              <a className="w-full">
+                <Button text="로그인" bgblack bgcolor="bg-purple-500"></Button>
+              </a>
+            </Link>
+            <Link href="/profile/signup">
+              <a className="w-full">
+                <Button text="회원가입" bgcolor="bg-white"></Button>
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
