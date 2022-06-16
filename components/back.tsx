0980e094280da3d2acfd2b163ce068f45443bb2f
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-06-16 13:40:14 +0900

    2022.06.16

--- components/back.tsx
+++ components/back.tsx
@@ -1,24 +1,31 @@
+import Link from "next/link";
+
 interface BackProps {
   text: string;
+  where: string;
 }
 
-export default function Back({ text }: BackProps) {
+export default function Back({ text, where }: BackProps) {
   return (
     <div className="flex px-4 py-2 w-full items-center justify-start font-bold box-border fixed">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 mr-5 cursor-pointer"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M10 19l-7-7m0 0l7-7m-7 7h18"
-        />
-      </svg>
+      <Link href={where}>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 mr-5 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+        </a>
+      </Link>
       <h1>{text}</h1>
     </div>
   );
--- pages/profile/signup.tsx
+++ pages/profile/signup.tsx
@@ -1,7 +1,64 @@
 import { NextPage } from "next";
+import Back from "../../components/back";
+import Label from "../../components/label";
+import Input from "../../components/input";
+import Button from "../../components/button";
+import Link from "next/link";
 
 const Signup: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Back where="/enter" text="회원가입"></Back>
+      <div className="w-full h-screen flex flex-col justify-between items-center px-3 box-border pt-16">
+        <form className="flex flex-col w-full sm:w-96 2xl:w-1/3 box-border">
+          <Label htmlfor="id" text="아이디"></Label>
+          <Input inputname="id" inputtype="text"></Input>
+          <Label htmlfor="bimil" text="비밀번호"></Label>
+          <Input inputname="bimil" inputtype="password"></Input>
+          <Label htmlfor="birth" text="생년월일"></Label>
+          <Input inputname="birth" inputtype="text"></Input>
+          <span className="text-xs py-4">
+            계정을 생성하기 위하여 다음에 동의하여 주시기 바랍니다.
+          </span>
+          <div className="flex items-center text-xs md:text-sm pb-4 gap-2">
+            <input
+              className="w-4 h-4"
+              type="checkbox"
+              value="true"
+              name="service"
+            />
+            <label htmlFor="service">
+              Twitch의 서비스 약관에 동의하여 주시기 바랍니다.
+            </label>
+          </div>
+          <div className="flex items-center text-xs md:text-sm pb-4 gap-2">
+            <input
+              className="w-7 h-7"
+              type="checkbox"
+              value="true"
+              name="personal"
+            />
+            <label htmlFor="personal">
+              개인정보 수집항목, 이용목적, 보유기간이 설명되어 있는 Twitch의
+              개인정보 처리방침을 읽고 이해하였습니다.
+            </label>
+          </div>
+          <Button text="로그인" bgcolor="bg-slate-200" large={true}></Button>
+        </form>
+        <div className="sm:w-96 2xl:w-1/3 h-1/3 flex flex-col justify-center text-xs md:text-sm">
+          <span>
+            상호명: TwitchInteractive, Inc. | 대표자: Emmett Shear, CEO | 주소:
+            350 Bush St, Suite 200, San Francisco, California, 94104, USA |
+            전화번호: 070-8984-5697 | 호스팅 서비스 제공자 : Amazon Web
+            Services, Inc.
+          </span>
+          <span className="cursor-pointer pt-4 font-semibold text-purple-600 text-xs md:text-sm ">
+            지원팀에 문의하기 | 판매약관 | 사업자정보
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
