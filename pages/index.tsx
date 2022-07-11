e1b6a75854dbfd629e404a234f3c08b1e9e77354
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-07-11 18:03:42 +0900

    2022.07.11

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import useUser from "../libs/useUser";
 
 const Home: NextPage = () => {
   const user = useUser();
-  return <div></div>;
+  return <div>
+    
+  </div>;
 };
 
 export default Home;
